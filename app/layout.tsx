07d5e6c407e303bd78d00c783ba5bd52e58fec9f
StefanObie <stefanus.oberholzer@gmail.com>
2026-09-08 21:55:12 +0200

    feat: rebrand app as Gauteng North Biathlon, hide user email/logout from nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "SA Biathlon Race Day",
+  title: "Gauteng North Biathlon",
   description: "Race-day results system for SA Biathlon leagues",
   icons: {
     icon: "/favicon.ico",
